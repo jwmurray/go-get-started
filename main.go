e8e9b3293cc8ae53b38987f363fc859af9be6f0e
jwmurray <john@gardenway.org>
2020-11-17 09:10:53 -0700

    fifth

--- main.go
+++ main.go
@@ -7,8 +7,5 @@ func main() {
 	fmt.Println("  Congratulations. You started successfully !")
 	fmt.Println("  You'll see, Go is simple and really slick.")
 	fmt.Println("")
-	fmt.Println("  You can modify this program by tweaking `main.go`")
-	fmt.Println("  You can modify this program by tweaking `main.go`")
-	fmt.Println("  You can modify this program by tweaking `main.go`")
 	fmt.Println("")
 }
