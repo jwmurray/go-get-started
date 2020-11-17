26acf2fe0b2c163e5cafec9d1674e8d8f5469abc
jwmurray <john@gardenway.org>
2020-11-17 09:13:41 -0700

    sixth

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println("")
 	fmt.Println("  Congratulations. You started successfully !")
-	fmt.Println("  You'll see, Go is simple and really slick.")
 	fmt.Println("")
 	fmt.Println("")
 }
